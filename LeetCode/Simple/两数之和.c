aca550ed95d2f55db8dfe738c88867efb5b0414c
yyhdreaM-dot <3417992175@qq.com>
2026-07-10 18:52:51 +0800

    Update 两数之和.c

--- LeetCode/Simple/两数之和.c
+++ LeetCode/Simple/两数之和.c
@@ -11,11 +11,11 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
                 int* ret = malloc(sizeof(int) * 2);
                 ret[0] = i;
                 ret[1] = j;
-                *returnSize = 2;
+                *returnSize = 2;//为returnSize赋值 返回int类型数据的个数
                 return ret;
             }
         }
     }
-    *returnSize == 0;
+    *returnSize == 0;returnSize为零说明未找到 指针为空
     return NULL;
 }
