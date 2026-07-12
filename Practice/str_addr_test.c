5995eb717bcda73f3685695a64e8c13f91960d99
AETK <3417992175@qq.com>
2026-07-12 17:49:36 +0800

    Update str_addr_test.c

--- Practice/str_addr_test.c
+++ Practice/str_addr_test.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 int main(){
+    
+    //定义两个字符数组 栈区各自分配独立内存
     char str1[] = "hello world";
     char str2[] = "hello world";
+    
+    //定义两个字符串常量 指向只读文字常量区
     const char* str3 = "hello world";
     const char* str4 = "hello world";
 
+    //数组名代表数组首元素地址 两个数组内存独立 地址不同
     if(str1 == str2)
     printf("str1 and str2 are same\n");
     else
     printf("str1 and str2 are not same\n");
+
+    //相同字符串两个指针可以共同使用 两个指针地址相等
     if(str3 == str4)
     printf("str3 and str4 are same\n");
     else
     printf("str13 and str4 are not same\n");
+    
     return 0;
 }
